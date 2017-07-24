c69435d3743c31f8c1d4d87b7308ed7ca6c03f76
dmitry <dmitry.zyryanov@execution.su>
2017-07-24 18:29:51 +0700

    Chart waiting animation #40

--- frontend/public/js/etherwallet.js
+++ frontend/public/js/etherwallet.js
@@ -899,11 +899,78 @@ function onload() {
             });
     };
 
+    function createChartWaiting(ctx) {
+        const SLICES_PER_PERIOD = 10;
+        const PERIODS = 3;
+
+        function makeData(start) {
+            const SLICES = Math.floor(PERIODS * SLICES_PER_PERIOD) + 1;
+            const labels = new Array(SLICES).fill('');
+            const values = new Array(SLICES).fill(null).map((_, i) => Math.sin(start + 2 * Math.PI * i / SLICES_PER_PERIOD));
+            return {
+                labels,
+                values
+            };
+        }
+
+        const initData = makeData(0);
+
+        const options = {
+            type: 'line',
+            data: {
+                labels: initData.labels,
+                datasets: [
+                    {
+                        data: initData.values,
+                        borderWidth: 1,
+                        pointRadius: 0
+                    }
+                ]
+            },
+            options: {
+                scales: {
+                    xAxes: [{
+                        display: false
+                    }],
+                    yAxes: [{
+                        display: false
+                    }]
+                },
+                legend: false,
+                responsive: false
+            }
+        };
+
+        let chart = new Chart(ctx, options);
+
+        function update() {
+            if (!chart) {
+                return;
+            }
+            const data = makeData(+new Date() / 1000);
+            chart.data.datasets[0].data = data.values;
+            chart.update();
+            setTimeout(update, 300)
+        }
+        update();
+
+        return {
+            destroy() {
+                chart.destroy();
+                chart = null;
+            }
+        };
+    }
+
     const chartCtx = Page.getChartCanvasElement();
     if (!chartCtx) {
         console.log('No chart canvas element');
     } else {
+
+        const wc = createChartWaiting(chartCtx);
+
         Ether.getTokensHistory((err, data) => {
+            wc.destroy();
             if (err) {
                 throw `Tokens history error, ${err}`;
             }
